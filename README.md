364b058a2518b8d5e6d7baeb14934f0fc4c9470b
kaneki <niccolo.malgeri@stud.unifi.it>
2023-06-13 20:04:48 +0200

    added README and attack test

---
+++ README.md
@@ -0,0 +1,30 @@
+# Introduction
+
+Steamrush is a 2d metroidvania-inspired platform game in which you play as a steampunk ferret trying to escape from an
+old laboratory full of dangers.
+
+# Things you can do
+
+Run away to save yourself
+
+![[room1clip.gif]]
+
+Use powerful droids to destroy your enemies
+
+![[clipShellDroid1.gif]]
+
+Exploit their abilities at your advantage
+
+![[clipShellDroid2.gif]]
+
+Each one with its own use
+
+![[clipGlidingDroid.gif]]
+
+Heal your wounds with tacos of dubious origins
+
+![[clipTaco.gif]]
+
+Show your worth and complete the steamrush
+
+![[clipBoss.gif]]
--- test/EnemyTest.cpp
+++ test/EnemyTest.cpp
@@ -13,6 +13,7 @@ public:
     std::unique_ptr<Movement> automovement;
     std::vector<AttackTarget *> targets;
     std::list<AttackTarget *> targetsList;
+    std::vector<AttackTarget *> enemyTargets;
     std::unique_ptr<Bullet> projectile;
 
 
@@ -26,7 +27,7 @@ public:
         player = std::make_unique<GameCharacter>(50, 50);
         auto playerTexture = new sf::Texture;
         playerTexture->loadFromFile("./images/playerSheet.png");
-        std::unique_ptr<Movement> playerMovement = std::make_unique<FlyingMovement>(80, sf::Vector2f(50.f, 50.f),
+        std::unique_ptr<Movement> playerMovement = std::make_unique<FlyingMovement>(80, sf::Vector2f(500.f, 500.f),
                                                                                     sf::Vector2f(50, 50),
                                                                                     player->spritePointer(), true);
         std::unique_ptr<Attack> playerAttack = std::make_unique<MeleeAttack>(sf::Vector2f(45.f, 35.f), 0.5f, 1, 49.f,
@@ -47,7 +48,7 @@ public:
                         tile->getGlobalBounds().height),
                                                                                enemy->spritePointer(),
                                                                                sf::Vector2f(16, 16));
-        std::unique_ptr<Attack> enemyAttack = std::make_unique<AutoMelee>(sf::Vector2f(60.f, 60.f), 0.5f, 1, 49.f,
+        std::unique_ptr<Attack> enemyAttack = std::make_unique<AutoMelee>(sf::Vector2f(600.f, 600.f), 0.5f, 1, 49.f,
                                                                           0.0f, 0.f,
                                                                           player->spritePointer());
         auto enemyAnimation = std::make_unique<Animation>(playerTexture, sf::Vector2i(5, 3), 0.3f, sf::Vector2f(35, 35),
@@ -60,6 +61,8 @@ public:
         targetsList.push_back(player->generateTarget());
         enemy->addWalls(walls);
         enemy->addTargets(targets);
+        enemyTargets.push_back(enemy->generateTarget());
+        player->addTargets(enemyTargets);
         projectile = std::make_unique<Bullet>(sf::Vector2f(50.f, 50.f), 100.f, 1.f, 20.f, playerTexture, 3, 30000.f);
     }
 };
@@ -72,7 +75,7 @@ TEST_F(EnemyTest, BulletTest) {
 
 }
 
-TEST_F(EnemyTest, Enemytest) {
+TEST_F(EnemyTest, PathFinderTest) {
     player->setPosition(100.f, 50.f);
     enemy->setPosition(400.f, 50.f);
     targets.push_back(player->generateTarget());
@@ -82,7 +85,7 @@ TEST_F(EnemyTest, Enemytest) {
     while ((std::abs(player->getPosition().x - enemy->getPosition().x) > 20.f or
             std::abs(player->getPosition().y - enemy->getPosition().y) > 20.f) and
            time.getElapsedTime().asSeconds() < 10) {
-        player->update(0.01, walls, player->getPosition());
+        player->update(0.01f, walls, player->getPosition());
         enemy->update(0.01f, walls, player->getPosition());
     }
 
@@ -90,10 +93,21 @@ TEST_F(EnemyTest, Enemytest) {
     player->update(0.01f, walls, player->getPosition());
 
 
-
-
     EXPECT_TRUE(std::abs(player->getPosition().x - enemy->getPosition().x) <= 25.f and
                 std::abs(player->getPosition().y - enemy->getPosition().y) <= 25.f);
 
 
+}
+
+TEST_F(EnemyTest, AttackTest) {
+    float startHp = player->getHp();
+    player->setPosition(100.f, 50.f);
+    enemy->setPosition(100.f, 50.f);
+    for (int i = 0; i < 3; i++) {
+        player->update(0.01f, walls, player->getPosition());
+        enemy->update(0.01f, walls, player->getPosition());
+        sf::sleep(sf::seconds(
+                0.5f)); //necessario poichè l'attacco utilizza un sistema di delay per sincronizzarsi con l'animazione
+    }
+    EXPECT_TRUE(player->getHp() < startHp);
 }
